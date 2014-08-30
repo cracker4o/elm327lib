28f91df2112cd0e695ce27aae006a7dfa559c89d
Tosho Toshev <tosho_az@yahoo.com>
2014-08-29 22:17:34 -0400

    The Short term fuel trim command is implemented

---
+++ ElmCommunicator/ElmCommunicator/Commands/ObdIICommands/ShowCurrentData/ShortTermFuelTrimSendMessage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElmCommunicator.Commands.ObdIICommands.ShowCurrentData
+{
+    public enum FuelBanks
+    {
+        FirstFuelBank = 1,
+        SecondFuelBank = 2
+    }
+
+    public class ShortTermFuelTrimSendMessage : SendMessage
+    {
+        private const string ShowCurrentDataCommand = "01";
+        private const string FirstFuelBankCommand = "06";
+        private const string SecondFuelBankCommand = "08";
+
+        public ShortTermFuelTrimSendMessage(FuelBanks fuelBank)
+        {
+            this.Command = ShowCurrentDataCommand;
+
+            switch (fuelBank)
+            {
+                case FuelBanks.FirstFuelBank:
+                    this.Data = FirstFuelBankCommand;
+                    break;
+
+                case FuelBanks.SecondFuelBank:
+                    this.Data = SecondFuelBankCommand;
+                    break;
+            }
+        }
+    }
+}
---
+++ ElmCommunicator/ElmCommunicatorTests/Commands/ObdIICommands/ShowCurrentData/ShortTermFuelTrimSendMessageTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using ElmCommunicator.Commands;
+using ElmCommunicator.Commands.ObdIICommands.ShowCurrentData;
+using ElmCommunicatorTests.Commands.ObdIICommands.ShowCurrentData;
+using NUnit.Framework;
+using Rhino.Mocks;
+
+namespace ElmCommunicatorTests.Commands.ObdIICommands.ShowCurrentData
+{
+    [TestFixture]
+    public class ShortTermFuelTrimSendMessageTests
+    {
+        private ShortTermFuelTrimSendMessage _messageToSend;
+
+        [Test]
+        public void ShouldSendABank1Command()
+        {
+            const string expectedMessageData = "06";
+            this._messageToSend = new ShortTermFuelTrimSendMessage(FuelBanks.FirstFuelBank);
+            Assert.AreEqual(expectedMessageData, this._messageToSend.Data);
+        }
+
+        [Test]
+        public void ShouldSendBank2Command()
+        {
+            const string expectedMessageData = "08";
+            this._messageToSend = new ShortTermFuelTrimSendMessage(FuelBanks.SecondFuelBank);
+            Assert.AreEqual(expectedMessageData, this._messageToSend.Data);
+        }
+    }
+}
