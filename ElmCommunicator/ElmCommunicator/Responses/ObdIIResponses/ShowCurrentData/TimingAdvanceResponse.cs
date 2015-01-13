4a80b227639a849894cac91f6ddc7e2b2368a084
Tosho Toshev <tosho.toshev@emerson.com>
2015-01-12 19:33:21 -0500

    The timing advance command is added

---
+++ ElmCommunicator/ElmCommunicator/Responses/ObdIIResponses/ShowCurrentData/TimingAdvanceResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ElmCommunicator.Commands;
+
+namespace ElmCommunicator.Responses.ObdIIResponses.ShowCurrentData
+{
+    public class TimingAdvanceResponse : ResponseMessage
+    {
+        public override IReceiveMessage Parse(string message)
+        {
+            this.Command = this.GetCommand(ref message);
+            this.Data = message.Substring(4);
+            TimingAdvance = (this.HexToDec(this.Data) - 128f) / 2;
+            return this;
+        }
+
+        public double TimingAdvance { get; private set; }
+    }
+}
---
+++ ElmCommunicator/ElmCommunicatorTests/Responses/ObdIIResponses/ShowCurrentData/TimingAdvanceResponseTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ElmCommunicator.Responses.ObdIIResponses.ShowCurrentData;
+
+using NUnit.Framework;
+
+namespace ElmCommunicatorTests.Responses.ObdIIResponses.ShowCurrentData
+{
+    [TestFixture]
+    public class TimingAdvanceResponseTests
+    {
+        private TimingAdvanceResponse _timingAdvanceResponse;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _timingAdvanceResponse = new TimingAdvanceResponse();
+        }
+
+        [Test]
+        public void ShouldSetTheCommand()
+        {
+            string message = "41 0E 45";
+            string expectedCommand = "410E";
+            var result = _timingAdvanceResponse.Parse(message);
+            Assert.AreEqual(expectedCommand, result.Command);
+        }
+
+        [Test]
+        public void ShouldSetTheData()
+        {
+            string message = "41 0E 45";
+            string expectedData = "45";
+            var result = _timingAdvanceResponse.Parse(message);
+            Assert.AreEqual(expectedData, result.Data);
+        }
+
+        [Test]
+        public void ShouldSetTheTimingAdvanceProperty()
+        {
+            string message = "41 0E 45";
+            float expectedTimingAdvance = (0x45 - 128f) / 2;
+            var result = _timingAdvanceResponse.Parse(message);
+            Assert.AreEqual(expectedTimingAdvance, ((TimingAdvanceResponse)result).TimingAdvance);
+        }
+    }
+}
